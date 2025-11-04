cded50ed95e2e05393100b7aa09b1d0dfa3c65ca
sahil <sahilsharma22200@gmail.com>
2025-11-04 15:36:26 +0530

    merged changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,3 @@
-
 import AnnualReport from "@/components/about/AnnualReport";
 import Health from "@/components/about/Health";
 import MissionVision from "@/components/about/MissionVision";
@@ -10,27 +9,24 @@ import LeaderShip from "@/components/about/LeaderShip";
 import LatestNews from "@/components/home/LatestNews";
 import Contact from "@/components/home/Contact";
 
-
-
 const About = async () => {
-     // const res = await fetch(`${process.env.NEXT_PUBLIC_APP_BASE_URL}/pages/home`);
-     console.log(data);
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_APP_BASE_URL}/pages/home`);
+  console.log(data);
 
-     // const res = await fetch(`/data/aboutus.json`);
-     // if (!res.ok) {
-     //   throw new Error("Failed to fetch layout data");
-     // }
-     // const { data } = await res.json();
-     // if (!data) return null;
-     // const homePageData = data.content;
+  // const res = await fetch(`/data/aboutus.json`);
+  // if (!res.ok) {
+  //   throw new Error("Failed to fetch layout data");
+  // }
+  // const { data } = await res.json();
+  // if (!data) return null;
+  // const homePageData = data.content;
 
+  console.log("about page", data);
 
-     console.log("about page", data);
-
-     return (
-          <div>
-               {/* Cookie code */}
-               {/* <div className="flex flex-col gap-2.5">
+  return (
+    <div>
+      {/* Cookie code */}
+      {/* <div className="flex flex-col gap-2.5">
                <div className="text-center text-2xl font-medium leading-[100%] "> 
                     Cookie Preferences
                </div>
@@ -46,33 +42,24 @@ const About = async () => {
                     <Button variant="outline">Recect All</Button>
                </div>
                </div> */}
-               {/* Cookie code */}
-               <InnerBanner data={data?.banner} />
-               <Health data={data?.health} />
-               {data.qualityCare &&
-                    <QualityCare qualityCare={data?.qualityCare} />}
-               {data.mission &&
-                    <MissionVision mission={data?.mission} />
-               }
+      {/* Cookie code */}
+      <InnerBanner data={data?.banner} />
+      <Health data={data?.health} />
+      {data.qualityCare && <QualityCare qualityCare={data?.qualityCare} />}
+      {data.mission && <MissionVision mission={data?.mission} />}
 
-               {data?.leadership &&
-                    <LeaderShip leadership={data?.leadership} />
-               }
-               {data?.annualReport &&
-                    <AnnualReport annualReport={data?.annualReport} />
-
-               }
-               {data?.latestNews &&
-                    <LatestNews data={data?.latestNews} />
-               }
-               {data?.contact &&
-                    <Contact
-                         data={data?.contact}
-                    // isLoading={isLoading}
-                    // isError={isError}
-                    // error={error}
-                    />}
-          </div>
-     );
+      {data?.leadership && <LeaderShip leadership={data?.leadership} />}
+      {data?.annualReport && <AnnualReport annualReport={data?.annualReport} />}
+      {data?.latestNews && <LatestNews data={data?.latestNews} />}
+      {data?.contact && (
+        <Contact
+          data={data?.contact}
+          // isLoading={isLoading}
+          // isError={isError}
+          // error={error}
+        />
+      )}
+    </div>
+  );
 };
 export default About;
